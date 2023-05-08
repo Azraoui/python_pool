4ea1e28e32ebc943c755e610f939d60a8c04f1c5
Abdellah El Azraoui <ael-azra@student.1337.ma>
2023-05-08 18:12:13 +0100

    working on day00 solve ex00

---
+++ day00/ex00/Hello.py
@@ -0,0 +1,9 @@
+ft_list = ["Hello", "World!"]
+ft_tuple = ("Hello", "Benguerir!")
+ft_set = {"Hello", "Morocco!"}
+ft_dict = {"Hello" : "1337_bg!"}
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
